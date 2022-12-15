35864e50d7e3ed1a6305daf887d2acb8bf088950
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-15 12:03:45 +0100

    Taking Decisions: if / else Statements

--- script.js
+++ script.js
@@ -159,7 +159,6 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
-*/
 
 ////////////////////////////////////
 // Strings and Template Literals
@@ -183,3 +182,26 @@ lines');
 console.log(`String
 multiple
 lines`);
+*/
+
+////////////////////////////////////
+// Taking Decisions: if / else Statements
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license 🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+let century;
+
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
