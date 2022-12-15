8f3a9571ecd2b96fec8489ab7c2b7561966ab5e1
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-15 12:22:52 +0100

    Equality Operators: == vs. ===

--- script.js
+++ script.js
@@ -260,7 +260,6 @@ console.log('23' / '2');
 let n = '1' + 1; 
 n = n - 1;
 console.log(n);
-*/
 
 ////////////////////////////////////
 // Truthy and Falsy Values
@@ -285,3 +284,29 @@ if (height) {
 } else {
     console.log('Height is UNDEFINED');
 }
+*/
+
+////////////////////////////////////
+// Equality Operators: == vs. ===
+
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
+
+if (age == 18) console.log('You just became an adult :D (loose)');
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+// 22 === 23 -> FALSE
+if (favourite === 23) {
+    console.log('Cool! 23 is an amzaing number!')
+} else if (favourite === 7) {
+    console.log('7 is also a cool number')
+} else if (favourite === 9) {
+    console.log('9 is also a cool number')
+} else {
+    console.log('Number is not 23 or 7 or 9')
+}
+
+if (favourite !== 23) console.log('Why not 23?');
