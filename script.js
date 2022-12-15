7053231d8f78ce24a9bd882e7d952bb64424dd34
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-15 13:08:40 +0100

    The switch Statement

--- script.js
+++ script.js
@@ -352,6 +352,7 @@ TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 GOOD LUCK 😀
 */
 
+/*
 // const scoreDolphins = (96 + 108 + 89) / 3;
 // const scoreKoalas = (88 + 91 + 110) / 3;
 // console.log(scoreDolphins, scoreKoalas);
@@ -377,4 +378,48 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
     console.log('Both win the trophy!');
 } else {
     console.log('No one wins the trophy 😭');
+}
+*/
+
+////////////////////////////////////
+// The switch Statement
+
+const day = 'friday';
+switch (day) {
+    // day === 'monday'
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
 }
